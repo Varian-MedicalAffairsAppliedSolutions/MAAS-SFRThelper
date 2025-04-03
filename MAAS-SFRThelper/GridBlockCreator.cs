797bed0aab96ebde8f0641358d84eec2dda37321
Ryan Clark <112436993+Varian-RC@users.noreply.github.com>
2025-04-03 11:13:42 -0700

    Added QR Code

--- MAAS-SFRThelper/GridBlockCreator.cs
+++ MAAS-SFRThelper/GridBlockCreator.cs
@@ -23,7 +23,7 @@ namespace VMS.TPS
         // Define the project information for EULA verification
         private const string PROJECT_NAME = "SFRThelper";
         private const string PROJECT_VERSION = "1.0.0";
-        private const string LICENSE_URL = "https://github.com/Varian-MedicalAffairsAppliedSolutions/MAAS-SFRThelper";
+        private const string LICENSE_URL = "https://varian-medicalaffairsappliedsolutions.github.io/MAAS-SFRThelper/";
         private const string GITHUB_URL = "https://github.com/Varian-MedicalAffairsAppliedSolutions/MAAS-SFRThelper";
 
         public Script()
