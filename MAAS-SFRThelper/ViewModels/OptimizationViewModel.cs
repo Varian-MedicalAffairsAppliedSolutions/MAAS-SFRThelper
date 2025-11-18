372059c9a3ebf918eb919f2ab3631ef93e304073
japnp <japankpatel@hotmail.com>
2025-11-18 18:57:06 +0000

    started developing optimization tab

--- MAAS-SFRThelper/ViewModels/OptimizationViewModel.cs
+++ MAAS-SFRThelper/ViewModels/OptimizationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MAAS_SFRThelper.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace MAAS_SFRThelper.ViewModels
         public OptimizationViewModel(EsapiWorker esapi)
         {
             _esapiworker = esapi;
+         
         }
     }
 }
